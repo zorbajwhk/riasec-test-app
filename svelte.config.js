f2e3f43d6e331269ae2b2913e9b608af1ed4b4fc
Zorab Wong <me@zorbawong.com>
2025-07-01 21:03:59 +0900

    Update netlify adapater code

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,9 @@ import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: {
-    adapter: adapter({
-      edge: false,
-      split: false
-    })
-  }
+	kit: {
+		adapter: adapter()
+	}
 };
 
 export default config;
